c7b3e36a84d6eadf128cdb08968b6c4fd9341764
liwenwne <1004749546@qq.com>
2018-06-24 17:56:21 +0800

    2018-6-24

---
+++ js/detail.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay:false,//等同于以下设置
+  speed:3000,
+  pagination : '.swiper-pagination',
+  paginationHide :true,
+  paginationClickable :true,
+  loop : true,
+  speed:3000,
+});
